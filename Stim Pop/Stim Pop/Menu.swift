36c318c5e01bd82af9d438e369c181f97421919e
Narciso <fabrizio.narciso@gmail.com>
2023-09-21 12:01:27 -0300

    background

--- Stim Pop/Stim Pop/Menu.swift
+++ Stim Pop/Stim Pop/Menu.swift
@@ -10,27 +10,31 @@ import SwiftUI
 struct Menu: View {
     
     var body: some View {
-        VStack {
-            VStack{
-                Image("logo")
-                    .resizable()
-                    .scaledToFit()
-                    .padding(64)
-            }
+        GeometryReader { geometry in
             VStack {
-                Card(title: "Score Mode", description: "Match to beat your high score.", icon: "trophy")
-                    .padding(.bottom,-8)
-                Card(title: "Physics", description: "Play with the properties.", icon: "arrow.up.and.down.and.sparkles")
-                    .padding(.bottom,-8)
-                Card(title: "Wall Mode", description: "Infinite bubbles.", icon: "infinity")
-                    .padding(.bottom,-8)
-                Card(title: "About", description: "Game Credits.", icon: "info.circle",bottom: true)
+                Spacer()
+                VStack{
+                    Image("logo")
+                        .resizable()
+                        .scaledToFit()
+                        .padding(64)
+                }
+                VStack {
+                    Card(title: "Score Mode", description: "Match to beat your high score.", icon: "trophy")
+                        .padding(.bottom,-8)
+                    Card(title: "Physics", description: "Play with the properties.", icon: "arrow.up.and.down.and.sparkles")
+                        .padding(.bottom,-8)
+                    Card(title: "Wall Mode", description: "Infinite bubbles.", icon: "infinity")
+                        .padding(.bottom,-8)
+                    Card(title: "About", description: "Game Credits.", icon: "info.circle",bottom: true)
+                }
+                .background(
+                    RoundedRectangle(cornerRadius: 10.0)
+                        .foregroundColor(Color("Card"))
+                )
+                .padding(16)
+                Spacer()
             }
-            .background(
-                RoundedRectangle(cornerRadius: 10.0)
-                    .foregroundColor(Color("Card"))
-            )
-            .padding(16)
         }
         .background(Color("Background"))
         .ignoresSafeArea(.all)
